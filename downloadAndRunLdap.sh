6563f34cd034325a3ae1fc4a97a4cd09c572cef7
Petar <petar.ivanov@tufts.edu>
2024-05-10 17:07:52 -0400

    Modified downloadAndRunLdap.sh with URLs pointing to 01-ldap-T

--- downloadAndRunLdap.sh
+++ downloadAndRunLdap.sh
@@ -2,22 +2,22 @@
 
 mkdir ldap-seed-data
 cd ldap-seed-data
-wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/main/ldap-seed-data/01-users.ldif
-#wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/main/ldap-seed-data/03-more-users.ldif
-wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/main/ldap-seed-data/03-redirect.ldif
+wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/01-ldap-T/ldap-seed-data/01-users.ldif
+#wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/01-ldap-T/ldap-seed-data/03-more-users.ldif
+wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/01-ldap-T/ldap-seed-data/03-redirect.ldif
 cd ..
 mkdir schemas
 cd schemas
-wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/main/schemas/eduperson.ldif
+wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/01-ldap-T/schemas/eduperson.ldif
 cd ..
-wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/main/Dockerfile
-wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/main/subject.properties
-wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/main/grouper-loader.properties
+wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/01-ldap-T/Dockerfile
+wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/01-ldap-T/subject.properties
+wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/01-ldap-T/grouper-loader.properties
 docker build -t openldap-dinkel-grouper .
 docker run -d -p 389:389 --name openldap-dinkel-grouper --mount type=bind,source="$PWD/ldap-seed-data",target=/etc/ldap/prepopulate \
-   -e SLAPD_PASSWORD=secret -e SLAPD_CONFIG_PASSWORD=secret -e SLAPD_DOMAIN=tufts.edu openldap-dinkel-grouper
+   -e SLAPD_PASSWORD=secret -e SLAPD_CONFIG_PASSWORD=secret -e SLAPD_DO01-ldap-T=tufts.edu openldap-dinkel-grouper
 
-# wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/main/downloadAndRunLdap.sh
+# wget https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/01-ldap-T/downloadAndRunLdap.sh
 # chmod +x downloadAndRunLdap.sh
 # ./downloadAndRunLdap.sh
 # cleanup: 
@@ -27,4 +27,4 @@ docker run -d -p 389:389 --name openldap-dinkel-grouper --mount type=bind,source
 
 #DO NOT USE
 #https://raw.githubusercontent.com/Internet2/grouper/GROUPER_2_5_BRANCH/grouper-misc/openldap-dinkel-grouper/.. . Use below instead
-# https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/main/..
+# https://raw.githubusercontent.com/Tufts-Technology-Services/openldap-dinkel-grouper/01-ldap-T/..
