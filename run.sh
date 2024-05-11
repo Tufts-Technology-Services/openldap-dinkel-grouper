405ffb28c32eb7e5a4cb39e5ca0b0435f2acaba8
PeteH <138930555+petar-tufts@users.noreply.github.com>
2024-05-11 12:25:02 -0400

    Update run.sh
    fixing domain

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-docker run -d -p 389:389 --name openldap-dinkel-grouper --mount type=bind,source=$dirname/ldap-seed-data,target=/etc/ldap/prepopulate -e SLAPD_PASSWORD=secret -e SLAPD_CONFIG_PASSWORD=secret -e SLAPD_DOMAIN=example.edu openldap-dinkel-grouper
+docker run -d -p 389:389 --name openldap-dinkel-grouper --mount type=bind,source=$dirname/ldap-seed-data,target=/etc/ldap/prepopulate -e SLAPD_PASSWORD=secret -e SLAPD_CONFIG_PASSWORD=secret -e SLAPD_DOMAIN=tufts.edu openldap-dinkel-grouper
