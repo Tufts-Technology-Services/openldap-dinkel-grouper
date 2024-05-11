04390c980a5ea8ce3543b1b2e36b360c42c364cd
PeteH <138930555+petar-tufts@users.noreply.github.com>
2024-05-11 12:25:30 -0400

    Update run.bat
    fixing domain

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 
 Set dirname=%cd%
 
-docker run -d -p 389:389 --name openldap-dinkel-grouper --mount type=bind,source=%dirname%\ldap-seed-data,target=/etc/ldap/prepopulate -e SLAPD_PASSWORD=secret -e SLAPD_CONFIG_PASSWORD=secret -e SLAPD_DOMAIN=example.edu openldap-dinkel-grouper
+docker run -d -p 389:389 --name openldap-dinkel-grouper --mount type=bind,source=%dirname%\ldap-seed-data,target=/etc/ldap/prepopulate -e SLAPD_PASSWORD=secret -e SLAPD_CONFIG_PASSWORD=secret -e SLAPD_DOMAIN=tufts.edu openldap-dinkel-grouper
