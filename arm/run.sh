1fbbbe0673b9420fe0caa84774c1c259966cb438
PeteH <138930555+petar-tufts@users.noreply.github.com>
2024-05-13 09:13:37 -0400

    Update run.sh
    change domain

--- arm/run.sh
+++ arm/run.sh
@@ -5,5 +5,5 @@
 docker run -d -p 389:389 --name openldap-dinkel-grouper \
   openldap-dinkel-grouper:latest
 
-# -e SLAPD_PASSWORD=secret -e SLAPD_CONFIG_PASSWORD=secret -e SLAPD_DOMAIN=example.edu 
+# -e SLAPD_PASSWORD=secret -e SLAPD_CONFIG_PASSWORD=secret -e SLAPD_DOMAIN=tufts.edu 
 
